8272bd305121a8f23735696fc9fba7087b20f966
Jose Garcia <jagarcia7655@gmail.com>
2021-04-13 21:03:42 -0500

    Aprendiendo PD decorator

--- dist/Decorator.7fec3b2a.js
+++ dist/Decorator.7fec3b2a.js
@@ -117,79 +117,66 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
+})({"designPatterns/Decorator/index.ts":[function(require,module,exports) {
+var Field =
+/** @class */
+function () {
+  function Field(field) {
+    var _this = this;
 
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
+    this.field = field;
+    this.errors = [];
+    var errorMessage = document.createElement("p");
+    this.field.parentNode.insertBefore(errorMessage, this.field.nextSibling);
+    this.field.addEventListener("input", function () {
+      _this.errors = [];
+
+      _this.validate();
+
+      errorMessage.innerText = _this.errors[0] || "";
+    });
   }
 
-  return bundleURL;
-}
+  Field.prototype.validate = function () {};
 
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+  return Field;
+}();
 
-    if (matches) {
-      return getBaseURL(matches[0]);
+function RequiredFieldDecorator(field) {
+  var validate = field.validate;
+
+  field.validate = function () {
+    validate();
+    var value = field.field.value;
+
+    if (!value) {
+      field.errors.push("Lo siento, este campo no puede estar vacío");
     }
-  }
-
-  return '/';
-}
-
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
-}
-
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
   };
 
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
+  return field;
 }
 
-var cssTimeout = null;
+function EmailFieldDecorator(field) {
+  var validate = field.validate;
 
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
-  }
+  field.validate = function () {
+    validate();
+    var value = field.field.value;
 
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
+    if (value.indexOf("@") === -1) {
+      field.errors.push("Debe ser un email");
     }
+  };
 
-    cssTimeout = null;
-  }, 50);
+  return field;
 }
 
-module.exports = reloadCSS;
-},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"media-player/assets/style.css":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var field = document.getElementById("field");
+var fieldToValidate = new Field(field);
+fieldToValidate = RequiredFieldDecorator(fieldToValidate);
+fieldToValidate = EmailFieldDecorator(fieldToValidate);
+},{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -392,5 +379,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/style.70689c72.js.map
+},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","designPatterns/Decorator/index.ts"], null)
+//# sourceMappingURL=/Decorator.7fec3b2a.js.map
